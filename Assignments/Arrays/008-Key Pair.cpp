a5ad53dbddeed5a333d0a1bee8969ed454ff58df
tangent03 <amanawp03@gmail.com>
2025-01-06 11:04:39 +0530

    Update 008-Key Pair.cpp

--- Assignments/Arrays/008-Key Pair.cpp
+++ Assignments/Arrays/008-Key Pair.cpp
@@ -1,78 +1,40 @@
-								//JAI SHREE RAM
-								//JAI BAJRANGBALI
-								//OM NAMAH SHIVAYE
-								
-			//TIME COMPLEXITY - O(N)
-			//SPACE COMPLEXITY - O(1)
-			
-			
-//QUESTION : - Find The Union And Intersection Of Two SORTED arrays.
+#include<bits/stdc++.h>
 
-//IN INTERVIEWS - GO FROM BRUTE FORCE SOLUTION -> BETTER SOLUTION -> OPTIMAL SOLUTION
-
-
-//so in this we are making start as 0 and end as n-1
-//and then add first and last and compare if total sum is large reduce end--
-//if sum is less start++. if same return true;
-
-
-//THIS IS KEY PAIR SOLUTION NOT TWO SUM
-#include <bits/stdc++.h>
 using namespace std;
 
-//brute force
+const int MOD = 1e9 + 7;
 
-bool twoSumProblem(int arr[],int n,int k){
-	for(int i=0;i<n;i++){
-		for(int j=i+1;j<n;j++){
-			if(arr[i]+arr[j]==k){
-				return true;
-			}
-		}
-	}
-	return false;
+// Function to calculate GCD of two numbers
+int gcd(int a, int b) {
+    while (b != 0) {
+        int t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
 }
 
-bool twoPointerSolution(int arr[],int n,int k){
-	//isse phele sort krlena O(NlogN);
-	int start=0;
-	int end=n-1;
-	sort(arr,arr+n);
-	while(start<end){
-		if(arr[start] + arr[end] == k){
-			return true;
-		}
-		else if(arr[start] + arr[end] > k){
-			end--;
-		}
-		else{
-			start++;
-		}
-	
-	}
-	return false;
+// Function to find the sum of GCDs of all subsets
+int sumOfGcdOfSubsets(vector<int>& arr) {
+    int n = arr.size();
+    int totalSum = 0;
+    
+    // Loop over all possible subsets
+    for (int i = 1; i < (1 << n); i++) {
+        int subsetGcd = 0;
+        for (int j = 0; j < n; j++) {
+            if (i & (1 << j)) {
+                subsetGcd = __gcd(subsetGcd, arr[j]);
+            }
+        }
+        totalSum = (totalSum + subsetGcd) % MOD;
+    }
+    
+    return totalSum;
 }
 
 int main() {
-	int n;
-	cin>>n;
-	int k;
-	cin>>k;
-	int arr[n];
-	for(int i=0;i<n;i++){
-		cin>>arr[i];
-	}
-	
-	
-	for(int i=0;i<n;i++){
-		cout<<arr[i]<<" ";
-	}
-	cout<<endl;
-	bool answer = twoPointerSolution(arr,n,k);
-	
-	
-	cout<<" Do Two Sum Pair Exist? "<<answer<<endl;
-	return 0;
-    
+    vector<int> arr = {2,3,4};
+    cout << sumOfGcdOfSubsets(arr) << endl; // Output for the provided input
+    return 0;
 }
-
